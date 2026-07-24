7f89cd19d6afbc443428742d15a36951186310f7
tejaswinibhat11 <tejaswinibhat359@gmail.com>
2026-07-24 16:53:26 +0530

    added 3 more codes

---
+++ HalfPyramid34.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HalfPyramid34 {
+        public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        for(int i=1;i<=rows;i++){
+
+            for(int j=1;j<=cols;j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+}
