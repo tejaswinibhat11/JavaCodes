83ee90b0f10a3e448633440243f27bbef30a71f9
tejaswinibhat11 <tejaswinibhat359@gmail.com>
2026-07-24 14:33:16 +0530

    added simple  codes

---
+++ VowelorConsonent18.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VowelorConsonent18 {
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char ch = sc.next().charAt(0);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
+                || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
+    }
+}
