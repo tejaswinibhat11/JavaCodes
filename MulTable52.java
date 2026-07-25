1ea25425afab134831f7d5938a903854356a475c
tejaswinibhat11 <tejaswinibhat359@gmail.com>
2026-07-25 23:12:42 +0530

    1 code

---
+++ MulTable52.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MulTable52 {
+    public static void table(int n) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        table(n);
+    }
+}
