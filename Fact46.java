1304896f88bc5cc0cb7dac818ed18f9cb18d1698
tejaswinibhat11 <tejaswinibhat359@gmail.com>
2026-07-25 23:03:26 +0530

    added 1  code

---
+++ Fact46.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Fact46 {
+    public static int factorial(int n) {
+
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(factorial(n));
+    }
+}
