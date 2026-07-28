58bc002e80105a5ef914172ed8a60556e5c9478c
tejaswinibhat11 <tejaswinibhat359@gmail.com>
2026-07-28 05:45:25 +0530

    ...

--- CelsiustoFahrenheit.java
+++ CelsiustoFahrenheit.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
-public class CelsiustoFahrenheit
- {
+public class CelsiustoFahrenheit {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         double celsius = sc.nextDouble();
-
+        // Fotmula
         double fahrenheit = (celsius * 9 / 5) + 32;
 
         System.out.println("Fahrenheit = " + fahrenheit);
